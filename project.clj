6c6178319f9ea185e3542494b0436263010c42af
Russell Whitaker <rwhitaker@ulive.com>
2015-06-09 22:32:20 -0700

    Updated hadoop dependencies

--- project.clj
+++ project.clj
@@ -10,8 +10,8 @@
                  [com.esotericsoftware/kryo "3.0.1"]]
   :profiles {:provided
                {:dependencies
-                 [[org.apache.hive/hive-exec "0.12.0"]
-                 [org.apache.hive/hive-serde "0.12.0"]
+                 [[org.apache.hive/hive-exec "1.2.0"]
+                 [org.apache.hive/hive-serde "1.2.0"]
                  [org.apache.hadoop/hadoop-core "1.2.1"]]}
              :dev
                {:dependencies [[speclj "3.2.0"]
