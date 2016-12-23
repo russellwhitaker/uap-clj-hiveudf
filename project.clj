68bd61c3dc38872a11d0601e03b613a52274196d
Russell Whitaker <rwhitaker@ulive.com>
2016-12-23 12:34:53 -0800

    Updated for uap-clj v1.3.0 dependency

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject uap-clj-hiveudf "1.2.2"
+(defproject uap-clj-hiveudf "1.3.0"
   :description "Apache Hadoop Hive GenericUDF wrapper around uap-clj"
   :url "https://github.com/russellwhitaker/uap-clj-hiveudf"
   :license {:name "The MIT License (MIT)"
@@ -6,14 +6,14 @@
   :scm {:name "git"
         :url "https://github.com/russellwhitaker/uap-clj-hiveudf"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [uap-clj "1.2.2"]]
+                 [uap-clj "1.3.0"]]
   :exclusions [org.apache.hadoop/hadoop-core
                org.apache.hadoop/hadoop-common
                org.apache.hadoop/hadoop-hdfs]
   :profiles {:provided
                {:dependencies
-                 [[org.apache.hive/hive-exec "2.1.0"]
-                  [org.apache.hive/hive-serde "2.1.0"]
+                 [[org.apache.hive/hive-exec "2.1.1"]
+                  [org.apache.hive/hive-serde "2.1.1"]
                   [org.apache.hadoop/hadoop-core "1.2.1"]]}
              :dev
                {:dependencies [[speclj "3.3.2"]
@@ -21,7 +21,7 @@
                 :test-paths ["spec"]}}
   :plugins [[lein-git-deps   "0.0.2"]
             [lein-ancient    "0.6.10"]
-            [lein-bikeshed   "0.3.0"]
+            [lein-bikeshed   "0.4.1"]
             [jonase/eastwood "0.2.3"]
             [speclj          "3.3.2"]]
   :git-dependencies [["https://github.com/ua-parser/uap-core.git"]]
